850db4bfeb72eb86344328c18489eff28c986a95
Ahsan Tonmoy <ahosant82@gmail.com>
2025-04-08 16:25:25 +0600

    add title for project

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,13 @@ import { Button } from "./components/ui/button";
 function App() {
   return (
     <>
-      <div className=" bg-amber-300 w-fit ">
-        <Button>Click me</Button>
+      <div className="title p-6">
+        <h1 className="text-3xl font-semibold capitalize ">
+          demo project for frontend project
+        </h1>
+      </div>
+      <div className=" bg-amber-300 w-fit mx-auto ">
+        <Button>add me</Button>
       </div>
     </>
   );
