b5ffc7f0ad823c7f0a35b4d5d3ba568650cff80f
Ahsan Tonmoy <ahosant82@gmail.com>
2025-04-08 16:18:29 +0600

    project initialzed with shadcn ui

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className=" bg-amber-300 w-fit ">
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
